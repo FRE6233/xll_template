60c8feb5ff6c76ca36a8cda1011a1146cda0c416
keithalewis <kal@kalx.net>
2022-09-10 15:21:58 +0000

    hw2

--- xll_template.cpp
+++ xll_template.cpp
@@ -1,15 +1,10 @@
 ﻿// xll_template.cpp - Sample xll project.
-#include <cmath> // for double tgamma(double)
 #include "xll_template.h"
-
-
-
-
 
 using namespace xll;
 
-AddIn xai_moneynes(
-	Function(XLL_DOUBLE, "xll_moneyness", "MONEYNESS")
+AddIn xai_moneyness(
+	Function(XLL_DOUBLE, "xll_moneyness", "NORMAL.MONEYNESS")
 	.Arguments({
 		Arg(XLL_DOUBLE, "f", "is the forwar4d."),
 		Arg(XLL_DOUBLE, "s", "is the vol."),
@@ -22,32 +17,7 @@ double WINAPI xll_moneyness(double f, double s, double k)
 	return moneyness(f, s, k);
 }
 
-AddIn xai_tgamma(
-	// Return double, C++ name of function, Excel name.
-	Function(XLL_DOUBLE, "xll_tgamma", "TGAMMA")
-	// Array of function arguments.
-	.Arguments({
-		Arg(XLL_DOUBLE, "x", "is the value for which you want to calculate Gamma.")
-	})
-	// Function Wizard help.
-	.FunctionHelp("Return the Gamma function value.")
-	// Function Wizard category.
-	.Category("MATH")
-	// URL linked to `Help on this function`.
-	.HelpTopic("https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/tgamma-tgammaf-tgammal")
-	.Documentation(R"xyzyx(
-The <i>Gamma</i> function is \(\Gamma(x) = \int_0^\infty t^{x - 1} e^{-t}\,dt\), \(x \ge 0\).
-If \(n\) is a natural number then \(\Gamma(n + 1) = n! = n(n - 1)\cdots 1\).
-<p>
-Any valid HTML using <a href="https://katex.org/" target="_blank">KaTeX</a> can 
-be used for documentation.
-)xyzyx")
-);
-// WINAPI calling convention must be specified
-double WINAPI xll_tgamma(double x)
-{
-#pragma XLLEXPORT // must be specified to export function
-
-	return tgamma(x);
-}
-
+//!!! implement xll_normal_pdf as NORMAL.PDF using normal_pdf
+//!!! implement xll_normal_cdf as NORMAL.CDF using normal_pdf
+//!!! implement xll_normal_cumulant as NORMAL.CUMULANT using normal_cumlant
+//!!! implement xll_black_put as BLACK.PUT using black_put
--- xll_template.h
+++ xll_template.h
@@ -1,10 +1,47 @@
 // xll_template.h - common includes
 #pragma once
-// Uncomment to build for pre 2007 Excel
-//#define XLOPERX XLOPER
+#define _USE_MATH_DEFINES // for M_PI
+#include <cmath>
 #include "xll/xll/xll.h"
 
+// F = f exp(-s^2/2 + s Z) <= k if and only if Z <= (log(k/f) + s^2/2)/s
 inline double moneyness(double f, double s, double k)
 {
 	return (log(k / f) + s * s / 2) / s;
+}
+
+inline double normal_pdf(double z)
+{
+	return exp(-z * z / 2) / sqrt(2 * M_PI);
+}
+
+inline double normal_cdf(double z)
+{
+	return 0*z; //!!! use erf
+}
+
+// n-th derivative of the standard normal cumulant
+inline double normal_cumulant(double s, unsigned int n = 0)
+{
+	if (n == 0) {
+		return s * s / 2;
+	}
+	else if (n == 1) {
+		return s;
+	}
+	else if (n == 2) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+inline double black_put(double f, double s, double k)
+{
+	double z = moneyness(f, s, k);
+	double N = 0*z; //!!! P(F <= k) = P(X <= z)
+	double Ns = 0*z; //!!! P(Fe^{s^2/2} <= k) = P(X + s <= z) = P(X <= z - s)
+
+	return k * N - f * Ns;
 }
